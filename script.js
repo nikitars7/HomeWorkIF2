336494d9cef586083ec0b4d5f46e419589c29634
Nikita <nikita_rs7@ukr.net>
2022-10-24 15:37:27 +0300

    main

---
+++ script.js
@@ -0,0 +1,15 @@
+"use strict"
+
+let number = prompt("Введите любое число", "");
+
+if (+number > 0) {
+   alert('1');
+}
+if (+number === 0) {
+   alert('0');
+}
+
+else if (+number < 0) {
+   alert('-1');
+}
+
